f29324337494e8582b4e1028b03b16b44e2fc7a7
Lucas <lucasmbrute614@gmail.com>
2021-11-29 14:53:46 -0300

    Course JS 74: Manipulating datas

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const textMessage = document.querySelector('.message')
+let text = document.querySelector('.message')
+const numberToGuess = Math.trunc(Math.random()*20)+1
+const score = document.querySelector('.score')
+let scoreNumber =20
+// document.querySelector('.number').textContent = 40
+// document.querySelector('.guess').value = 40
+document.querySelector('.number').textContent = numberToGuess
+
+document.querySelector('.check').addEventListener('click',()=>{
+    const guess = +document.querySelector('.guess').value;
+    if(!guess){
+        text.textContent = 'No number!'
+    }else if (guess === numberToGuess){
+        textMessage.textContent = 'Sucess!'
+    }else if(guess>numberToGuess){
+        textMessage.textContent= "Too High"
+        scoreNumber --
+        score.textContent = scoreNumber
+    }else{
+        textMessage.textContent="Too low"
+        scoreNumber --
+        score.textContent = scoreNumber
+    }
+})
+
