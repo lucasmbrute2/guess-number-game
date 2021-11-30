af779f2ce25bfd2fe09e73f6872a9848c70a7d02
Lucas <lucasmbrute614@gmail.com>
2021-11-29 22:57:10 -0300

    JS course- 76: manipulating events

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
 'use strict';
 
-const textMessage = document.querySelector('.message')
+const text = document.querySelector('.message')
 const score = document.querySelector('.score')
 const numberToGuess = Math.trunc(Math.random()*20)+1
+const numberText = document.querySelector('.number')
+const body = document.querySelector('body')
 let scoreNumber =20
 // document.querySelector('.guess').value = 40
-document.querySelector('.number').textContent = numberToGuess
+
 
 document.querySelector('.check').addEventListener('click',()=>{
     const guess = +document.querySelector('.guess').value;
     if(!guess){
         text.textContent = 'No number!'
-    }
-    if(scoreNumber>0){
-        if (guess === numberToGuess){
-            textMessage.textContent = 'Sucess!'
-            document.querySelector('body').style.backgroundColor ='#60b347';
-            
-            document.querySelector('.number').style.width= '30rem'
-        }else if(guess>numberToGuess){
-            textMessage.textContent= "Too High"
-            scoreNumber --
-            score.textContent = scoreNumber
-        }else{
-            textMessage.textContent="Too low"
-            scoreNumber --
-            score.textContent = scoreNumber
-        }
     }else{
-        textMessage.textContent = 'You lost the game!'
+        if(scoreNumber>0){
+            if (guess === numberToGuess){
+                numberText.textContent = numberToGuess
+                
+                text.textContent = 'Sucess!'
+                
+                body.style.backgroundColor ='#60b347';              
+                
+                numberText.style.width= '30rem'
+            }else if(guess>numberToGuess){
+                text.textContent= "Too High"
+                scoreNumber --
+                score.textContent = scoreNumber
+            }else{
+                text.textContent="Too low"
+                scoreNumber --
+                score.textContent = scoreNumber
+            }
+        }else{
+            text.textContent = 'You lost the game!'
+        }
     }
+})
+
+document.querySelector('.again').addEventListener('click',()=>{
+    const guess = document.querySelector('.guess');
+    score.textContent = 20
+    guess.value = ''
+    text.textContent = 'Start guessing...'
+    numberText.textContent='?'
+    body.style.backgroundColor = '#222';
+    numberText.style.width = '15rem'
 })
 
